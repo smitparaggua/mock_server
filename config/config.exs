db21a728cd9016258448b5ba1c62af7b74a294cb
John Smith Paraggua <smitparaggua@gmail.com>
2018-07-03 14:13:59 +0800

    Removed local config

--- config/config.exs
+++ config/config.exs
@@ -20,15 +20,6 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# TODO remove this
-config :mock_server, MockServer.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  port: 5444
-  #hostname: "db",
-  #port: "5432"
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
