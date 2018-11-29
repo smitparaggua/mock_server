1cef0dcc869cfb2b945159cfe12ec81ae5ea1e5d
John Smith Paraggua <john.paraggua@PLAM05170170.local>
2018-11-29 12:00:47 +0800

    [Ongoing] Before upgrade to Phoenix 1.4

--- assets/js/components/server.jsx
+++ assets/js/components/server.jsx
@@ -3,6 +3,8 @@ import {Link} from "react-router-dom"
 import styled from "styled-components"
 import {Servers} from "api"
 import RunServerButton from "./run_server_button"
+import {confirmAlert} from "react-confirm-alert"
+import "react-confirm-alert/src/react-confirm-alert.css"
 
 const ServerContainer = styled.div`
   display: flex;
@@ -16,6 +18,22 @@ const Subtext = styled.div`
   color: gray;
 `
 
+const MenuItems = styled.div`
+  display: flex;
+  align-items: center;
+
+  div, i {
+    margin-right: 10px;
+    margin-left: 10px;
+  }
+`
+
+const DeleteButton = styled.i`
+  color: #a33;
+  font-size: 1.2em;
+  cursor: pointer;
+`
+
 export default class Server extends React.PureComponent {
 
   constructor(props) {
@@ -26,6 +44,7 @@ export default class Server extends React.PureComponent {
     this.startServer = this.startServer.bind(this)
     this.stopServer = this.stopServer.bind(this)
     this.toggleServer = this.toggleServer.bind(this)
+    this.deleteServer = this.deleteServer.bind(this)
   }
 
   toggleServer() {
@@ -47,6 +66,29 @@ export default class Server extends React.PureComponent {
       .then(() => this.setState({state: 'stopped'}))
   }
 
+  deleteServer(id) {
+    const options = {
+      title: 'Title',
+      message: 'Message',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () => alert('Click Yes')
+        },
+        {
+          label: 'No',
+          onClick: () => alert('Click No')
+        }
+      ],
+      childrenElement: () => <div />,
+      customUI: ({ title, message, onClose }) => <div>Custom UI</div>,
+      willUnmount: () => {}
+    };
+    confirmAlert(options);
+    // show dialogue
+    // send delete request
+  }
+
   render() {
     const server = this.props.server
     return (
@@ -59,9 +101,14 @@ export default class Server extends React.PureComponent {
           </Subtext>
         </div>
 
-        <RunServerButton state={this.state.state} onClick={() => this.toggleServer()}>
-          Start
-        </RunServerButton>
+        <MenuItems>
+          <RunServerButton state={this.state.state} onClick={() => this.toggleServer()}>
+            Start
+          </RunServerButton>
+          <DeleteButton className={`fa fa-times-circle`}
+                        onClick={() => this.deleteServer(server.id)}>
+          </DeleteButton>
+        </MenuItems>
       </ServerContainer>
     )
   }
